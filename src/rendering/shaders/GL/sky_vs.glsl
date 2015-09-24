3cd7447c3533477930cbb30ecb48bedb8d14a4de
eugene sturm <eugene.sturm@code42.com>
2015-09-24 12:10:20 -0500

    - remove unused shader and try out interface block

--- src/rendering/shaders/GL/sky_vs.glsl
+++ src/rendering/shaders/GL/sky_vs.glsl
@@ -1,14 +1,21 @@
- #version 410                               
+#version 410
+
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 tex;
+
 uniform mat4 world;
-out vec3 tex_coord0;
+
+out VS_OUT {
+    vec3 tex_coord0;
+} vs_output;
+
 out gl_PerVertex {
-  vec4 gl_Position;
+    vec4 gl_Position;
 };
+
 void main() {
    vec4 world_pos = world * vec4(pos, 1.f);
    gl_Position = world_pos.xyww;
-   tex_coord0 = pos;
+   vs_output.tex_coord0 = pos;
 
 }
