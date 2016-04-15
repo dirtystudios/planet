e9c97a78735c62f89956a27cddd38bd831f36d8c
Jake <jarveson@gmail.com>
2016-04-15 00:50:17 -0500

    [vs2015] Port terrain shader and update project files for newest changes

--- src/rendering/shaders/DX11/terrain_vs.hlsl
+++ src/rendering/shaders/DX11/terrain_vs.hlsl
@@ -4,6 +4,9 @@ cbuffer cbPerObject : register ( b0 ) {
 	float4x4 view : VIEWPROJECTION;
 	float4x4 proj : PROJECTION;
 	float4x4 world : WORLD;
+	float4x4 trans;
+	float4x4 scale;
+	float rradius;
 	int elevations_tile_index;
 	int normals_tile_index;
 };
@@ -26,31 +29,23 @@ struct VS_OUTPUT {
 
 VS_OUTPUT VSMain( VS_INPUT Input ) {    
 	VS_OUTPUT output;
-	float height = 250.f * terrainHeightTileArray.SampleLevel(heightmapElevationsSampler, float3(Input.vTex, elevations_tile_index), 0);
+	float height = 0.f * terrainHeightTileArray.SampleLevel(heightmapElevationsSampler, float3(Input.vTex, elevations_tile_index), 0);
 	float3 normal = terrainNormalTileArray.SampleLevel(heightmapNormalsSampler, float3(Input.vTex, normals_tile_index), 0).xyz;
-    //float3 normal = {1.0f, 1.0f, 1.0f};
-    float4 pos = float4(Input.vPos.x, Input.vPos.y, height, 1.f);                  
-    output.vNormal = normal;
-    output.vTexture = Input.vTex;
 
-    /*
-    // in order to sphereicalize the cube, need to be -1 > x > 1, -1 > y > 1, -1 > z > 1
-    float radius = 500;
-    vec4 mapping = pos;    
-
-    float x_squared = pos.x * pos.x;
-    float y_squared = pos.y * pos.y;
-    float z_squared = pos.z * pos.z;
-    mapping.x = radius * pos.x * sqrt(1.f - (y_squared / 2.f) - (z_squared / 2.f) + (y_squared * z_squared / 3.f));
-    mapping.y = radius * pos.y * sqrt(1.f - (z_squared / 2.f) - (x_squared / 2.f) + (z_squared * x_squared / 3.f));
-    mapping.z = (radius + height) * pos.z * sqrt(1.f - (x_squared / 2.f) - (y_squared / 2.f) + (x_squared * y_squared / 3.f));
-    */  
+	float4 pos = float4(Input.vPos.x, Input.vPos.y, rradius / 2.f, 1.f);
+	pos = mul(scale, pos);
+	pos = mul(trans, pos);
 	
+	float3 mapped = normalize(pos.xyz) * (height + (rradius / 2.f));
+	
+	pos = float4(mapped.xyz, 1.f);
 	pos = mul(world, pos);
 	pos = mul(view, pos);
 	pos = mul(proj, pos);
 	
 	output.vPosition = pos;
+	output.vNormal = normal;
+    output.vTexture = Input.vTex;
 	
 	return output;
 }    
