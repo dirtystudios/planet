49924e230c128841a9e8862ca2907ecceb3b351b
Eugene Sturm <sturm049@gmail.com>
2016-11-03 16:54:04 -0700

    Submodule was awkwardly messed up. Unremove function impl.

--- src/utilities/osx/File_osx.cpp
+++ src/utilities/osx/File_osx.cpp
@@ -69,3 +69,30 @@ bool fs::exists(const std::string& path) {
     struct stat st;
     return stat(path.c_str(), &st) == 0;
 }
+
+std::vector<std::string> fs::ListFilesInDirectory(const std::string& path) {
+    std::vector<std::string> fnames;
+    DIR*                     dirFile = opendir(path.c_str());
+    if (dirFile) {
+        struct dirent* hFile;
+        
+        while (( hFile = readdir( dirFile )) != NULL )
+        {
+            if ( !strcmp( hFile->d_name, "."  )) continue;
+            if ( !strcmp( hFile->d_name, ".." )) continue;
+            
+            // in linux hidden files all start with '.'
+            if ( hFile->d_name[0] == '.' ) continue;
+            
+            // dirFile.name is the name of the file. Do whatever string comparison
+            // you want here. Something like:
+            //            if ( strstr( hFile->d_name, ".txt" ))
+            if (hFile->d_type == DT_REG) {
+                fnames.push_back(hFile->d_name);
+            }
+            //  printf( "%s\n", hFile->d_name);
+        }
+        closedir(dirFile);
+    }
+    return fnames;
+}
