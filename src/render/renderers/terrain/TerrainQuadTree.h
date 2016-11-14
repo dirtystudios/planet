914ec0e60ad4fd16426379b4665518614359a21c
Eugene Sturm <sturm049@gmail.com>
2016-11-14 15:47:57 -0800

    Move Deformation classes to own class

--- src/render/renderers/terrain/TerrainQuadTree.h
+++ src/render/renderers/terrain/TerrainQuadTree.h
@@ -4,37 +4,7 @@
 #include <memory>
 #include "Rectangle.h"
 #include "TerrainTileKey.h"
-
-enum class TerrainDeformationType : uint8_t { None, Spherical };
-
-class TerrainDeformation {
-private:
-    const TerrainDeformationType _type;
-
-public:
-    TerrainDeformation(TerrainDeformationType type) : _type(type) {}
-
-    virtual glm::vec3 worldToDeformed(const glm::vec3& world) = 0;
-};
-
-class NoDeformation : public TerrainDeformation {
-public:
-    NoDeformation() : TerrainDeformation(TerrainDeformationType::None) {}
-
-    virtual glm::vec3 worldToDeformed(const glm::vec3& world) final { return world; }
-};
-
-class SphericalDeformation : public TerrainDeformation {
-private:
-    float _radius{0.f};
-
-public:
-    SphericalDeformation(float radius) : TerrainDeformation(TerrainDeformationType::Spherical), _radius(radius) {}
-
-    virtual glm::vec3 worldToDeformed(const glm::vec3& world) final { return glm::normalize(world) * _radius; }
-
-    float radius() const { return _radius; };
-};
+#include "Deformation.h"
 
 class TerrainQuadNode;
 
